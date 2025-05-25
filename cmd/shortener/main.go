2ae7ecc65647e78bef44488b0969cb0c27b3fb82
Artem Goryachev <initx@mail.ru>
2025-05-26 00:05:17 +0300

    decodeHandler impemented
    + err shadowing

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -16,44 +16,62 @@ func main() {
 
 func run() error {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", shortenHandler)
+	mux.HandleFunc("POST /", encodeHandler)
+	mux.HandleFunc("GET /{id}/", decodeHandler)
 	return http.ListenAndServe("localhost:8080", mux)
 }
 
-func shortenHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		return
-	}
-
+func encodeHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Header.Get("Content-Type") != "text/plain" {
 		log.Println("Invalid content-type, expected string")
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
-	if body, err := io.ReadAll(r.Body); err != nil {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
 		log.Println("Error reading body:", err)
 		w.WriteHeader(http.StatusBadRequest)
 		return
-	} else {
-		log.Println("New request received:", string(body))
+	}
 
-		if _, err = url.ParseRequestURI(string(body[:])); err != nil {
-			log.Println("Could not parse URI: ", err)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
+	log.Println("New request received:", string(body))
 
-		shortURI := base64.StdEncoding.EncodeToString(body)
-		w.WriteHeader(http.StatusCreated)
+	if _, err = url.ParseRequestURI(string(body[:])); err != nil {
+		log.Println("Could not parse URI: ", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 
-		if _, err := w.Write([]byte(r.Host + "/" + shortURI)); err != nil {
-			log.Println("Could not write URI: ", shortURI)
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
+	shortURI := base64.StdEncoding.EncodeToString(body)
+	w.WriteHeader(http.StatusCreated)
+
+	if _, err := w.Write([]byte(r.Host + "/" + shortURI)); err != nil {
+		log.Println("Could not write URI: ", shortURI)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
 	}
 
 	return
+}
+
+func decodeHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Header.Get("Content-Type") != "text/plain" {
+		log.Println("Invalid content-type, expected string")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	shortURI := r.PathValue("id")
+	log.Println("PathValue:", shortURI)
+	longURI, err := base64.StdEncoding.DecodeString(shortURI)
+
+	if err != nil {
+		log.Println("Could not decode URI: ", err)
+	}
+	if _, err := w.Write(longURI); err != nil {
+		log.Println("Could not write URI: ", shortURI)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 }
